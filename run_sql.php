abcd220a0de82fb9388a74ac8cc79d100e15ee2b
drnjomy <lovegodvalue@gmail.com>
2026-07-26 15:27:25 +0300

    Remove temp password update script

--- run_sql.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/config.php';
-require_once __DIR__ . '/includes/database.php';
-$db = Database::getInstance();
-$db->query("UPDATE admin_users SET password_hash = ? WHERE username = ?", ['$2y$12$GEB0MTBOrflNknsE7y7JgeocJB1ewzIX7o5HWkkYwcueKPxlHCD7u', 'admin']);
-echo 'Password updated. New login: admin / :WaIP88kX)i76h';
